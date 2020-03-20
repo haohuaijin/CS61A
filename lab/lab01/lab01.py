deeca1117381c7e803c5603d790f970410082ff2
hao <1291536750@qq.com>
2020-03-20 21:50:49 +0800

    add proj lab

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,26 @@
+"""Lab 1: Expressions and Control Structures"""
+
+def both_positive(a, b):
+    """Returns True if both a and b are positive.
+
+    >>> both_positive(-1, 1)
+    False
+    >>> both_positive(1, 1)
+    True
+    """
+    return a and b > 0 # You can replace this line!
+
+def sum_digits(x):
+    """Sum all the digits of x.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
