d267890522916c33067599be9f356695fef587ae
Hao <47685845+wxh-123@users.noreply.github.com>
2020-04-21 21:28:09 +0800

    finish guerrilla 01

--- discussion/disc04.py
+++ discussion/disc04.py
@@ -6,6 +6,7 @@ def count_stair_ways(n):
         return count_stair_ways(n-1) + count_stair_ways(n-2)
 
 
+
 # Q 1.2
 def count_k(n, k):
     """
@@ -18,16 +19,100 @@ def count_k(n, k):
     >>> count_k(300, 1) # Only one step at a time
     1
     """
-    if n < 0:
-        return 0
-    elif n == 0 or n == 1:
+    if n == 1 or n == 0:
         return 1
-    elif k == 0:
+    elif n < 0:
         return 0
     else:
+        return sum([count_k(n-i, k) for i in range(1,k+1)])    
+ 
 
-        
-        return count_k(n-k, k) + count_k(n, k-1)    
+
+
+# Q 2.1
+"""
+>>> a = [1, 5, 4, [2, 3], 3]
+>>> print(a[0], a[-1])
+1 3
+>>> len(a)
+5
+>>> 2 in a
+False
+>>> 4 in a
+True
+>>> a[3][0]
+2
+"""
+
+
+# Q 2.2
+def even_weighted(s):
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted(x)
+    [0, 6, 20]
+    """
+    return [i * s[i] for i in range(len(s)) if i % 2 == 0]
+
+
+
+# Q 2.3 
+def max_product(s):
+    """Return the maximum product that can be formed using non-consecutive
+    elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    if len(s) < 1:
+        return 1
+    elif len(s) == 1:
+        return s[0]
+    else:
+        # 认真思考什么要由最大值
+        first = s[0] * max(max_product(s[2:]), max_product(s[3:]))
+        second = s[1] * max(max_product(s[3:]), max_product(s[4:]))
+        return max(first, second)
+
+
+
+# Whole Numbers
+# 1. hole number
+def check_hole_number(n):
+    """
+    >>> check_hole_number(123)
+    False
+    >>> check_hole_number(3241968)
+    True
+    >>> check_hole_number(3245968)
+    False
+    """
+    if n % 1000 // 100 > n % 100 // 10:
+        return check_hole_number(n // 100)
+    return n < 10
+
+# 2. mountain number
+def check_mountain_number(n):
+    """
+    >>> check_mountain_number(103)
+    False
+    >>> check_mountain_number(153)
+    True
+    >>> check_mountain_number(123456)
+    True
+    >>> check_mountain_number(2345986)
+    True
+    """
+    def helper(m, flag):
+                
+
+
+
+    return helper(n, (n % 10) > (n % 100 // 10))
+
 
 
 
---
+++ guerrilla/guer01.py
@@ -0,0 +1,210 @@
+# Q 1.1
+"""
+>>> lst = [1,2,3,4,5]
+>>> lst[1:3]
+[1,2]
+
+>>> lst[0:len(lst)]
+[1,2,3,4,5]
+
+>>> lst[-4:]
+[2,3,4,5]
+
+>>> lst[3:]
+[4,5]
+
+>>> lst[1:4:2]
+[2,4]
+
+>>> lst[:4:2]
+[1,4]
+
+>>> lst[1::2]
+[2,4]
+
+>>> lst[::-1]
+[5,4,3,2.1]
+
+>>> lst + 100
+error
+
+>>> lst3 = [[1], [2], [3]]
+>>> lst + lst3
+[1,2,3,4,5,[1],[2],[3]]
+"""
+
+
+# Q 1.2
+# 画图略过
+
+# Q 1.3
+def map_mut(f, L):
+    """
+    >>> L = [1, 2, 3, 4]
+    >>> map_mut(lambda x: x**2, L)
+    >>> L
+    [1, 4, 9, 16]
+    """
+    for i in range(0,len(L)):
+        L[i] = f(L[i])
+
+# Q 1.4
+"""
+# 1.4.1
+if you use 
+l = [1,2,3]
+s = l
+this is copying a pointer
+
+if you use
+l = [1,2,3]
+s = l.copy()
+this is a new list
+
+# 1.4.2
+use list.copy()
+"""
+
+
+
+# Q 1.5 
+#??? 有疑问
+"""
+1. the base case
+2. the stop case
+3. the recursice condition
+"""
+
+# Q 1.6
+# because this is have a stop case
+#???
+
+
+# Q 1.7
+# 识别递归三要素
+
+
+# Q 1.8
+# 画图
+
+
+# Q 1.9
+def cascade(n):
+    print(n)
+    if n >= 10:
+        cascade(n // 10)
+        print(n)
+"""
+>>> cascade(4)
+123
+12
+1
+12
+123
+"""
+
+
+# Q 1.10
+def paths(m, n):
+    """
+    >>> paths(2,2)
+    2
+    >>> paths(117, 1)
+    1
+    """
+    def helper(i, j):
+        right, up = 0, 0
+        if i < m - 1:
+            up = helper(i+1, j)
+        else:
+            return 1
+        if j < n - 1:
+            right = helper(i, j+1)
+        else:
+            return 1
+        return right + up
+    return helper(0, 0)
+
+
+
+# Q 1.11
+def merge(s1, s2):
+    """ Merge two sorted lists
+    >>> merge([1,3], [2,4])
+    [1,2,3,4]
+    >>> merge([1,2],[])
+    [1,2]
+    """
+    def helper(a, b, res):
+        if len(a) < 1:
+            return res + b
+        if len(b) < 1:
+            return res + a
+        
+        if a[0] <= b[0]:
+            res += [a[0]]
+            a = a[1:]
+        else:
+            res += [b[0]]
+            b = b[1:]
+        return helper(a, b, res)
+    return helper(s1, s2, [])
+
+
+
+# Q 1.12
+def mario_number(level):
+    """Return the number of ways that Mario can perform a sequence of steps
+    or jumps to reach the end of the level without ever landing in a Piranha
+    plant. Assume that every level begins and ends with a dash.
+    >>> mario_number('-P-P-') # jump, jump
+    1
+    >>> mario_number('-P-P--') # jump, jump, step
+    1
+    >>> mario_number('--P-P-') # step, jump, jump
+    1
+    >>> mario_number('---P-P-') # step, step, jump, jump or jump, jump, jump
+    2
+    >>> mario_number('-P-PP-') # Mario cannot jump two plants
+    0
+    >>> mario_number('----') # step, jump ; jump, step ; step, step, step
+    3
+    >>> mario_number('----P----')
+    9
+    >>> mario_number('---P----P-P---P--P-P----P-----P-')
+    180
+    """
+    if len(level) <= 1:
+        return 1
+
+    f, s, t = 0, 0, 0
+    if level[:3] == "-P-":
+        f = mario_number(level[2:])
+    if level[:3] == "---":
+        s = mario_number(level[2:])
+    if level[:2] == "--":
+        t = mario_number(level[1:])
+
+    return f + s + t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
