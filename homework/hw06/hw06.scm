3fa01600c7ca6eb88e898356f76137da212e13e4
Hao <47685845+wxh-123@users.noreply.github.com>
2020-06-28 16:01:40 +0800

    hw06 except Q5

--- homework/hw06/hw06.scm
+++ homework/hw06/hw06.scm
@@ -5,26 +5,30 @@
 ; Scheme
 
 (define (cddr s)
-  (cdr (cdr s)))
+  (cdr (cdr s))
+)
 
 (define (cadr s)
-  'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
 )
 
 (define (sign x)
-  'YOUR-CODE-HERE
+  (cond ((> x 0) 1) ((< x 0) -1) (else 0))
 )
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
+  (cond ((= n 1) b) 
+    ((odd? n) (* (pow b (- n 1)) b)) 
+    (else (square (pow b (/ n 2))))
+  )
 )
 
 (define (unique s)
-  'YOUR-CODE-HERE
+  ()
 )
