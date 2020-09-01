973c8199a7d9cf4e709dfab34e379e7c0c580952
hhj <1291536750@qq.com>
2020-09-01 16:01:51 +0800

    finish hw07

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -45,25 +45,41 @@
 (define (second-operand p) (caddr p))
 
 (define (derive-sum expr var)
-  'YOUR-CODE-HERE
+  (make-sum 
+    (derive (first-operand expr) var)
+    (derive (second-operand expr) var)
+  )
 )
 
 (define (derive-product expr var)
-  'YOUR-CODE-HERE
+  (make-sum
+    (make-product (derive (first-operand expr) var) (second-operand expr))
+    (make-product (first-operand expr) (derive (second-operand expr) var))
+  )
 )
 
 ; Exponentiations are represented as lists that start with ^.
 (define (make-exp base exponent)
-  'YOUR-CODE-HERE
+  (cond ((= exponent 0) 1)
+        ((= exponent 1) base)
+        ((number? base) (expt base exponent))
+        (else (list '^ base exponent))
+  )
 )
 
 (define (exp? exp)
-  'YOUR-CODE-HERE
+  (and (list? exp) (eq? (car exp) '^ ))
 )
 
 (define x^2 (make-exp 'x 2))
 (define x^3 (make-exp 'x 3))
 
 (define (derive-exp exp var)
-  'YOUR-CODE-HERE
+  (make-product
+    (second-operand exp)
+    (make-exp
+      var
+      (- (second-operand exp) 1)
+    )
+  )
 )
